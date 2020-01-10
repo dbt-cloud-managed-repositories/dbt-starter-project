075d50c449ae1a7e2d2ddd98e7c9b003d531e1e5
Drew Banin <drew@fishtownanalytics.com>
2020-01-10 11:45:58 -0500

    starter project

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,10 +1,12 @@
 
 -- Welcome to your first dbt model!
--- Did you know that you can also configure models directly within
--- the SQL file? This will override configurations stated in dbt_project.yml
+-- Did you know that you can also configure models directly within SQL files?
+-- This will override configurations stated in dbt_project.yml
 
--- Try changing 'view' to 'table', then re-running dbt
-{{ config(materialized='view') }}
+-- Try changing "table" to "view" below
+{{ config(materialized='table') }}
 
 
 select 1 as id
+union all
+select null as id

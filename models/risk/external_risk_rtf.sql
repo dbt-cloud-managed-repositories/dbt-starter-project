96c83a7baf21219b28b3f288ed24f81775a3ea10
Erik Klamer <erik.klamer@eye.security>
2023-09-07 17:01:23 +0000

    yes

--- models/risk/external_risk_rtf.sql
+++ models/risk/external_risk_rtf.sql
@@ -2,4 +2,32 @@
 {{ config(materialized='view') }}
 
 
-select a.eyed, f.id from spectrum.risk_rtf a, a.findings f
+select 
+root.eyed, 
+finding.id,
+finding.name,
+finding.identifier,
+finding.description_dev,
+finding.category,
+finding.destinations,
+finding.is_combi,
+finding.version,
+finding.scan_date,
+finding.translation_date,
+finding.translation_status,
+finding.motivation,
+finding.sources,
+finding.suffix,
+finding.penalty,
+finding.positive_score,
+finding.confidence,
+finding.risk_type,
+finding.data,
+finding.status,
+finding.url,
+finding.difficulty,
+finding.keymap,
+finding.raise_alert,
+finding.alert_severity,
+finding.executable
+from spectrum.risk_rtf root, root.findings finding
